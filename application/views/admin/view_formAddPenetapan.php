1d477b1b92aa79a59f3ad4515a5b1e72cb91e060
renom2098 <renom2098@gmail.com>
2023-10-17 08:17:52 +0700

    update di kampus

--- application/views/admin/view_formAddPenetapan.php
+++ application/views/admin/view_formAddPenetapan.php
@@ -5,30 +5,30 @@
     </div>
     <div class="modal-body">
 
-      <form class="form form-vertical" action="<?= base_url(); ?>admin/insert_dataPenetapan" method="post">
-          <div class="row">
-              <div class="col-12">
-                  <div class="mb-1">
-                      <label class="form-label" for="nama-peraturan">Nama Peraturan</label>
-                      <input type="text" id="nama-peraturan" class="form-control" name="f[nama_peraturan]" placeholder="Nama Peraturan">
-                  </div>
-              </div>
-              <div class="col-12">
-                  <div class="mb-1">
-                      <label class="form-label" for="jenis-peraturan">Jenis Peraturan</label>
-                      <input type="text" id="jenis-peraturan" class="form-control" name="f[jenis_peraturan]" placeholder="Jenis Peraturan">
-                  </div>
-              </div>
-              <div class="col-12">
-                  <div class="mb-1">
-                      <label class="form-label" for="nama_file">Nama File</label>
-                      <input type="text" id="nama_file" class="form-control" name="f[nama_file]" placeholder="Nama Peraturan">
-                  </div>
-              </div>
+        <form class="form form-vertical" action="<?= base_url(); ?>admin/insert_dataPenetapan" method="post">
+            <div class="row">
+                <div class="col-12">
+                    <div class="mb-1">
+                        <label class="form-label" for="nama-peraturan">Nama Peraturan</label>
+                        <input type="text" id="nama-peraturan" class="form-control" name="f[nama_peraturan]" placeholder="Nama Peraturan">
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="mb-1">
+                        <label class="form-label" for="jenis-peraturan">Jenis Peraturan</label>
+                        <input type="text" id="jenis-peraturan" class="form-control" name="f[jenis_peraturan]" placeholder="Jenis Peraturan">
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="mb-1">
+                        <label class="form-label" for="nama_file">Nama File</label>
+                        <input type="text" id="nama_file" class="form-control" name="f[nama_file]" placeholder="Nama Peraturan">
+                    </div>
+                </div>
 
-          </div>
-          <button type="submit" class="btn btn-primary flaot-end">Add</button>
-      </form>
+            </div>
+            <button type="submit" class="btn btn-primary flaot-end">Add</button>
+        </form>
     </div>
     
 </div>
