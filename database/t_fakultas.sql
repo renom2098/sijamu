a86ead8c41e97de8d46094b4e9e92eb88abc4fb1
renom2098 <renom2098@gmail.com>
2023-10-25 13:52:55 +0700

    new table

---
+++ database/t_fakultas.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 25, 2023 at 08:51 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sijamu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_fakultas`
+--
+
+CREATE TABLE `t_fakultas` (
+  `id` int(11) NOT NULL,
+  `nama_fakultas` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `t_fakultas`
+--
+
+INSERT INTO `t_fakultas` (`id`, `nama_fakultas`) VALUES
+(1, 'Fakultas Ilmu Administrasi'),
+(2, 'Fakultas Teknik'),
+(3, 'Fakultas Hukum'),
+(4, 'Fakultas Ilmu Komputer'),
+(5, 'Fakultas Ilmu Komunikasi'),
+(6, 'Fakultas Agro Bisnis dan Rekayasa Pertanian'),
+(7, 'Fakultas Keguruan dan Ilmu Pendidikan'),
+(8, 'Magister Administrasi Publik');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `t_fakultas`
+--
+ALTER TABLE `t_fakultas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `t_fakultas`
+--
+ALTER TABLE `t_fakultas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
